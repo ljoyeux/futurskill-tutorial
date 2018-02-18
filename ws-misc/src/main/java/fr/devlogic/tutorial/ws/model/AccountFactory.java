1173d9b6923fe94203cf788ac571d250dcfbfa0a
Laurent Joyeux <ljoyeux@free.fr>
2018-02-18 17:53:39 +0100

    Factory

---
+++ ws-misc/src/main/java/fr/devlogic/tutorial/ws/model/AccountFactory.java
@@ -0,0 +1,11 @@
+package fr.devlogic.tutorial.ws.model;
+
+public class AccountFactory {
+
+    public static Account account(String login) {
+        Account account = new Account();
+        account.setLogin(login);
+
+        return account;
+    }
+}
